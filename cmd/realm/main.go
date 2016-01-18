298c914b7345213862421ac2d8b07f94f4ef9bf8
brettlangdon <brett@blangdon.com>
2016-01-17 21:18:55 -0500

    add comments

--- cmd/realm/main.go
+++ cmd/realm/main.go
@@ -8,6 +8,7 @@ import (
 )
 
 func main() {
+	// Setup our CLI app
 	var app *cli.App = cli.NewApp()
 	app.Name = "realm"
 	app.Version = "0.1.0"
@@ -26,12 +27,16 @@ func main() {
 			Usage:  "'[<host>]:<port>' to bind too",
 		},
 	}
+
+	// This action is called for all commands
 	app.Action = func(c *cli.Context) {
+		// Ensure that a zone filename was provided
 		var filename string = c.String("zone")
 		if filename == "" {
 			log.Fatal("must supply zone file via \"--zone\" flag or \"REALM_ZONE\" environment variable")
 		}
 
+		// Load and parse the zone file
 		var zone *realm.Zone
 		var err error
 		log.Printf("parsing zone file \"%s\"\n", filename)
@@ -40,11 +45,13 @@ func main() {
 			log.Fatal(err)
 		}
 
+		// Create and start the server
 		var bind string = c.String("bind")
 		log.Printf("starting the server on \"%s\"\n", bind)
 		var server *realm.Server = realm.NewServer(bind, zone)
 		log.Fatal(server.ListenAndServe())
 	}
 
+	// Parse command arguments and run `app.Action`
 	app.RunAndExitOnError()
 }
--- server.go
+++ server.go
@@ -2,34 +2,45 @@ package realm
 
 import "github.com/miekg/dns"
 
+// A Server listens for DNS requests over UDP and responds with answers from the provided Zone.
 type Server struct {
 	server *dns.Server
 	zone   *Zone
 }
 
+// NewServer returns a new initialized *Server that will bind to listen and will look up answers from zone.
 func NewServer(listen string, zone *Zone) *Server {
-	var server *Server = &Server{zone: zone}
-	server.server = &dns.Server{
+	var s *Server
+	s = &Server{zone: zone}
+	s.server = &dns.Server{
 		Addr:    listen,
 		Net:     "udp",
-		Handler: server,
+		Handler: s,
 	}
-	return server
+	return s
 }
 
-func (server *Server) ListenAndServe() error {
-	return server.server.ListenAndServe()
+// ListenAndServe will start the nameserver on the configured address.
+func (s *Server) ListenAndServe() error {
+	return s.server.ListenAndServe()
 }
 
-func (server *Server) ServeDNS(w dns.ResponseWriter, request *dns.Msg) {
-	var response *dns.Msg = &dns.Msg{}
+// ServeDNS will be called for every DNS request to this server.
+// It will attempt to provide answers to all questions from the configured zone.
+func (s *Server) ServeDNS(w dns.ResponseWriter, request *dns.Msg) {
+	// Setup the default response
+	var response *dns.Msg
+	response = &dns.Msg{}
 	response.SetReply(request)
 	response.Compress = true
 
+	// Lookup answers to any of the questions
 	for _, question := range request.Question {
-		var records []dns.RR = server.zone.Lookup(question.Name, question.Qtype, question.Qclass)
+		var records []dns.RR
+		records = s.zone.Lookup(question.Name, question.Qtype, question.Qclass)
 		response.Answer = append(response.Answer, records...)
 	}
 
+	// Respond to the request
 	w.WriteMsg(response)
 }
